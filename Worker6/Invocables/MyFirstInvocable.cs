8aad432fdd46582ebf489971f7e19cd134c8102d
Alberto <aviezzi@outlook.com>
2022-05-13 11:47:24 +0200

    Add read write file

---
+++ Worker6/Invocables/MyFirstInvocable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Worker6.Invocables;
+
+using System.Text;
+using Coravel.Invocable;
+using Microsoft.Extensions.Options;
+using Worker5;
+
+public class MyFirstInvocable : IInvocable
+{
+    private readonly AppOptions _options;
+    private readonly ILogger<Worker> _logger;
+
+    public MyFirstInvocable(IOptions<AppOptions> options, ILogger<Worker> logger)
+    {
+        _options = options.Value;
+        _logger = logger;
+    }
+
+    
+    public async Task Invoke()
+    {
+        var lines = await File.ReadAllLinesAsync(@"C:\Sugar\sample\sample.txt");
+
+        await using (var fs = File.Create(@$"C:\Sugar\sample\{DateTime.Now:yyyyMMdd HHmmss}.txt"))
+        {
+            foreach (var line in lines)
+            {
+                var bytes = Encoding.UTF8.GetBytes(line);
+                await fs.WriteAsync(bytes);
+            }
+        }
+
+        var message = $"{_options.Text} at: {DateTimeOffset.Now}";
+        _logger.LogInformation(message);
+
+        await Task.Delay(1000);
+    }
+}
--- Worker6/Program.cs
+++ Worker6/Program.cs
@@ -1,6 +1,8 @@
+using Coravel;
 using Serilog;
 using Worker5;
 using Worker6;
+using Worker6.Invocables;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((services) =>
@@ -11,6 +13,8 @@ IHost host = Host.CreateDefaultBuilder(args)
             .CreateLogger();
         
         Log.Information("Hello world!");
+        
+        services.AddScheduler();
 
         var config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
@@ -18,16 +22,26 @@ IHost host = Host.CreateDefaultBuilder(args)
             .AddJsonFile("appsettings.json", false, true)
             .Build();
 
-        
         services.AddOptions<AppOptions>()
             .Bind(config.GetSection("AppOptions"));
-
-        services.AddHostedService<Worker>();
+        
+        // services.AddHostedService<Worker>();
+        services.AddSingleton<MyFirstInvocable>();
     })
     .ConfigureLogging(builder =>
     {
         builder.AddSerilog(Log.Logger);
     })
     .Build();
+
+host.Services.UseScheduler(scheduler =>
+{
+    scheduler
+        .Schedule<MyFirstInvocable>()
+        .EveryFiveSeconds()
+        .PreventOverlapping("lap1");
+});
+
+host.Run();
 
 await host.RunAsync();
