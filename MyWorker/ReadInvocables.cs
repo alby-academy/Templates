31b4e8d359194e802c361d6d10f812bc0fbfb8a9
MPilot93 <marco.plt.93@gmail.com>
2022-05-13 16:07:34 +0200

    fix write and reader, pre PullRequest

--- MyWorker/ReadInvocables.cs
+++ MyWorker/ReadInvocables.cs
@@ -25,21 +25,49 @@ namespace MyWorker
 
         public async Task Invoke()
         {
-            var lines = await File.ReadAllLinesAsync(_options.Dest);
-           
+            try 
+            { 
 
-            {
+            var lines = await File.ReadAllLinesAsync(_options.Dest);
+                await using (var fs = File.Create(_options.Source))
+
+                {
+                    List<Ordine> ordiniTot = new();
+                    foreach (var line in lines)
+                    {
+                        var ordine = new Ordine();
+                        var fileReader = line.Split(';').ToList();
+                        ordine.Name = fileReader[1];
+                        ordine.Model = fileReader[0];
+                        ordine.Value = Int32.Parse(fileReader[2]);
+                        ordiniTot.Add(ordine);
+
+                    }
+                    List<Ordine> result = ordiniTot.GroupBy(x => x.Name).Select(x => new Ordine
+                    {
+                        Name = x.First().Name,
+                        Model = x.First().Model,
+                        Value = x.First().Value
+                    })
+                       .ToList();
+
+                    foreach (var ordine in result)
+                    {
+                        var bytes = Encoding.UTF8.GetBytes(String.Concat($"{ordine.Name} {ordine.Value}", Environment.NewLine));
+                        await fs.WriteAsync(bytes);
+                    }
+                    _logger.LogInformation(@$"Created File: {_options.Source}\\{DateTime.Now:yyyyMMdd HHmmss}.txt");
+                    }
                 
-                 foreach (var line in lines)
-                 {
-                   var ordine = new Ordine();
-                    var fileReader = line.Split(';').ToList();
-                    ordine.Name = fileReader[1];
-                    ordine.Model = fileReader[0];
-                    ordine.Value = Int32.Parse(fileReader[2]);
-                 }
-                //await using (var fs = File.Create(_options.Source))
+
+                }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
 }
+
+
+
